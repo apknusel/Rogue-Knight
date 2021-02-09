539ed2560add1ffe4409606e16818cbc87b63aff
DemetriPapageorge <dgpapageorge@stu.naperville203.org>
2021-02-09 14:10:28 -0600

    restarted project in unity

---
+++ SE Semester Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float horizontal;
+    float vertical;
+    int currentHealth;
+
+    public float speed = 3.0f;
+    public int maxHealth = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rb.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        rb.MovePosition(position);
+    }
+
+    public int health()
+    {
+        return currentHealth;
+    }
+
+    public void changeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0 , maxHealth);
+    }
+}
