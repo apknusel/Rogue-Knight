734d7b5673e0c8c82c5a7b7a8603e1649c30b510
apknusel <apknusel@stu.naperville203.org>
2021-02-22 22:33:39 -0600

    Fixed animation for Player

--- SE Semester Project/Assets/Scripts/PlayerController.cs
+++ SE Semester Project/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
-    float horizontal;
-    float vertical;
     int currentHealth;
 
     public float speed = 3.0f;
     public int maxHealth = 100;
 
     public Animator animator;
-    Vector2 lookDirection = new Vector2(1, 0);
+
+    Vector2 movement;
 
     // Start is called before the first frame update
     void Start()
@@ -25,29 +24,18 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
 
-        Vector2 move = new Vector2(horizontal, vertical);
-
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-        {
-            lookDirection.Set(move.x, move.y);
-            lookDirection.Normalize();
-        }
-
-        animator.SetFloat("Look X", lookDirection.x);
-        animator.SetFloat("Look Y", lookDirection.y);
-        //animator.SetFloat("Speed", move.magnitude);
+        animator.SetFloat("Look X", movement.x);
+        animator.SetFloat("Look Y", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
         
     }
 
     void FixedUpdate()
     {
-        Vector2 position = rb.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
-        rb.MovePosition(position);
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
         Debug.Log(currentHealth);
     }
 
