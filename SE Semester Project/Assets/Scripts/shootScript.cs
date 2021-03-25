e7125688b6edef4a69d53c52bcccec7bc6aca4e7
apknusel <apknusel@stu.naperville203.org>
2021-03-25 14:04:18 -0500

    Worked on guns and stuff

---
+++ SE Semester Project/Assets/Scripts/shootScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootScript : MonoBehaviour
+{
+    public Transform Gun;
+
+    Vector2 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = mousePos - (Vector2)Gun.position;
+        FaceMouse();
+    }
+
+    void FaceMouse()
+    {
+        Gun.transform.right = direction;
+        if (Gun.transform.right.x < 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+    public Vector3 getRotation()
+    {
+        return Gun.transform.right;
+    }
+}
